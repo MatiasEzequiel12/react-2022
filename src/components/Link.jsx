7527e037275e688cea8b55181b710b3aa724f21b
MatiasEzequiel12 <matias_palaciio@hotmail.com>
2022-12-07 10:26:19 -0300

    Prueba

---
+++ src/components/Link.jsx
@@ -0,0 +1,14 @@
+const Link = () => {
+  return (
+    <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+      Que comienze el juego
+    </a>
+  )
+}
+
+export default Link;
